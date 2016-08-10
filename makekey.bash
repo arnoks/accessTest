9ecbc5b832934c6d476f0d9c273a092dee60338b
Arno K. Schamber <arno.schamber@deutsche-boerse.com>
2016-08-10 13:04:46 +0200

    Added some meaningful deafaults

--- makekey.bash
+++ makekey.bash
@@ -1,19 +1,45 @@
 #!/bin/bash
 PRIVATE_KEYFILE=~/.ssh/id_rsa
-PUBLIC_KEYFILE=~/.ssh/id_rsa.pub
+PUBLIC_KEYFILE=$PRIVATE_KEYFILE.pub
+COMMENT=$USER@$HOSTNAME
+GITHOST="github.com"
 
-if [[ -f $PRIVATE_KEYFILE && -f $PUBLIC_KEYFILE ]]
+function usage() 
+{
+    echo "makekeys  will generate keypair for $COMMENT"
+    echo "makekeys [comment] will generate keypair and assumes $GITHOST as target for test"
+    echo "makekeys [comment] [githost} will generate keypair and uses githost target for test"
+    echo "makekeys  -help || -h || -? prints this message"
+    exit 2
+}
+ 
+if [[ $1 = "-help" || $1 = "-h" || $1 = "-?" ]]
+then
+    usage
+fi
+
+if [[ ! -z $1 ]]
+then
+    COMMENT=$1
+fi
+
+if [[ ! -z $2 ]]
+then
+    COMMENT=$2
+fi
+
+if [[ -f $PRIVATE_KEYFILE ]]
 then
     echo "Key file already generate please install on github"
 else
     echo "Generating public/private key pair"
-    echo sh-keygen -t rsa -b 4096 -C "name@domain.com"
+    ssh-keygen -s -t rsa -b 4096 -C $COMMENT -f $PRIVATE_KEYFILE
 fi
 
-echo "\nPlease install now public key at github/setting\n"
+echo "\n Please install now public key at github/setting \n"
 cat $PUBLIC_KEYFILE
 
-read -p "\nPress Enter to continue ..."
+read -p "Press Enter to continue ..."
 
 read -p "Shall I add the key file to authentication agent yes/no? > "
 if [[ $REPLY == "y*" ]]
@@ -21,14 +47,14 @@ then
   ssh-add $PRIVATE_KEYFILE 
   if [[ $? -ne 0 ]]
   then
-      echo "error adding key to authenticaiton agent"
+      echo "Error adding key to authentication agent"
       exit 1
   fi
 fi
 
-echo "Executing access test using git user" 
+echo "Running test agiinst git@$GITHOST" 
 
-ssh -vT git@github.com
+ssh -vT git@$GITHOST
 
-echo "Please make sure to use the git@github.com/repository.git\n\
+echo "===== Caution ====== \n Please make sure to use the git@github.com/repository.git\n\
 in order to access repositoy. Yor user name will not work!"
